e19200734d2794b0671fc4157a90bcfb5531bea9
Evyatar <evyatar.a@fiverr.com>
2017-11-20 00:48:08 +0200

    1.5.3-rc.0 - allow server rendering

---
+++ webpack.config.universal.js
@@ -0,0 +1,42 @@
+const path = require('path'),
+    webpack = require('webpack'),
+    ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: {
+        index: ['./src/index.js']
+    },
+    externals: {
+        'react': 'react',
+        'throttle-debounce': 'throttle-debounce'
+    },
+    target: 'node',
+    output: {
+        path: path.join(__dirname, 'dist/universal'),
+        filename: '[name].js',
+        library: 'EmojiPicker',
+        libraryTarget: 'umd'
+    },
+    module: {
+        loaders: [{
+            test: /\.svg/,
+            loaders: ['svg-url-loader']
+
+        },
+        {
+            test: /\.scss$/,
+            use:ExtractTextPlugin.extract({fallback:"style-loader",use:["css-loader","sass-loader"]}),
+            include: path.resolve(__dirname, '../')
+        },
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+        }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextPlugin( "style.css" )
+    ]
+};
