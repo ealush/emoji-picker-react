e4c448b2aeaf25d61057fdee36e5f6dd839d4451
Anton Solodkov <anton.solodkov@sharekey.com>
2021-05-25 20:15:42 +0300

    make recent emojis update immediately

--- src/lib/clickHandler/index.js
+++ src/lib/clickHandler/index.js
@@ -1,14 +1,18 @@
 import { setRecentlyUsed } from '../recentlyUsed';
 import emojiOutput from '../emojiOutput';
+import { actionTypes } from '../reducer';
 
-const clickHandler = (onClickRef = {}) => (
+const clickHandler = (onClickRef = {}, dispatch) => (
   e,
   unified,
   emoji,
   activeSkinTone
 ) => {
   const output = emojiOutput(unified, emoji, activeSkinTone);
+
   setRecentlyUsed(output);
+  dispatch({ type: actionTypes.UPDATE_RECENTLY_USED });
+
   return onClickRef.current && onClickRef.current(e, output);
 };
 
