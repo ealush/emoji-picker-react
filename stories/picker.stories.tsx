6f41204c1d79104b83b88bf6bde41ebffeb8b0ce
ealush <code@ealush.com>
2022-08-17 00:47:43 +0300

    prepare deps

--- stories/picker.stories.tsx
+++ stories/picker.stories.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Thing, Props } from '../src';
+import { Picker, Props } from '../src';
 
 const meta: Meta = {
-  title: 'Welcome',
-  component: Thing,
+  title: 'Picker',
+  component: Picker,
   argTypes: {
     children: {
       control: {
@@ -19,7 +19,7 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story<Props> = args => <Thing {...args} />;
+const Template: Story<Props> = args => <Picker {...args} />;
 
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
