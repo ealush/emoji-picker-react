74dcd0c6e101194e08ef221f51c24bad01878fd3
ealush <code@ealush.com>
2022-08-21 01:06:38 +0300

    Add main picker ref

--- src/components/contextProvider/PickerContextProvider.tsx
+++ src/components/contextProvider/PickerContextProvider.tsx
@@ -1,11 +1,27 @@
 import * as React from 'react';
 
-const PickerContext = React.createContext({});
+const PickerContext = React.createContext<{
+  PickerMainRef: React.RefObject<HTMLElement>;
+}>({});
 
 type Props = Readonly<{
   children: React.ReactNode;
+  PickerMainRef: React.RefObject<HTMLElement>;
 }>;
 
-export function PickerContextProvider({ children }: Props) {
-  return <PickerContext.Provider value={{}}>{children}</PickerContext.Provider>;
+export function PickerContextProvider({ children, PickerMainRef }: Props) {
+  return (
+    <PickerContext.Provider
+      value={{
+        PickerMainRef: PickerMainRef
+      }}
+    >
+      {children}
+    </PickerContext.Provider>
+  );
+}
+
+export function usePickerMainRef() {
+  const { PickerMainRef } = React.useContext(PickerContext);
+  return PickerMainRef;
 }
