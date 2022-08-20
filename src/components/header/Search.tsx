311ba2ec25ca66e6d5f1c5470e1803d67e23a2ba
ealush <code@ealush.com>
2022-08-20 23:15:58 +0300

    Filter by first char

--- src/components/header/Search.tsx
+++ src/components/header/Search.tsx
@@ -1,10 +1,29 @@
 import * as React from 'react';
+import { createAlphaNumericEmojiIndex } from '../../dataUtils/createAlphaNumericEmojiIndex';
 import './Search.css';
 
 export function Search() {
   return (
     <div className="epr-search-container">
-      <input className="epr-search" type="text" placeholder="Search" />
+      <input
+        className="epr-search"
+        type="text"
+        placeholder="Search"
+        onChange={onChange}
+      />
     </div>
   );
+
+  function onChange({ target: { value } }) {
+    if (value.length === 0) {
+      // clear active filter
+    } else if (value.length === 1) {
+      createAlphaNumericEmojiIndex().then(emojis => {
+        console.log(emojis[value]);
+      });
+      // filter by first character
+    } else {
+      // filter by stack
+    }
+  }
 }
