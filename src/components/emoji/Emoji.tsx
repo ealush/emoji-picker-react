3367aed0adf69e5017408d0cbf835adb9ec1e71f
ealush <code@ealush.com>
2022-08-21 14:11:26 +0300

    Better Emoji Spacing

--- src/components/emoji/Emoji.tsx
+++ src/components/emoji/Emoji.tsx
@@ -4,19 +4,28 @@ import { DataEmoji } from '../../dataUtils/DataTypes';
 import { emojiUnified, emojiUrl } from '../../dataUtils/emojiSelectors';
 import { useIsEmojiFiltered } from '../../hooks/useFilter';
 import './Emoji.css';
+import { emojiColors } from './emojiColors';
 
 type Props = Readonly<{
   emoji: DataEmoji;
+  hidden: boolean;
+  index: number;
 }>;
 
-export function Emoji({ emoji }: Props) {
-  const hidden = useIsEmojiFiltered(emojiUnified(emoji));
+export function Emoji({ emoji, hidden, index }: Props) {
+  const color = bgColor(index);
+
   return (
     <button
       className={clsx('epr-emoji', { hidden })}
       data-unified={emojiUnified(emoji)}
+      style={{ color }}
     >
       <img src={emojiUrl(emoji)} className="epr-emoji-img" />
     </button>
   );
+}
+
+function bgColor(order: number): string {
+  return emojiColors[order % emojiColors.length];
 }
---
+++ src/components/emoji/emojiColors.ts
@@ -0,0 +1,13 @@
+const PASTEL_BLUE = '#aeeff0';
+const PASTEL_RED = '#ffe0e0';
+const PASTEL_GREEN = '#d6f8b8';
+const PASTEL_PURPLE = '#c6cfff';
+const PASTEL_YELLOW = '#f1f0d1';
+
+export const emojiColors = [
+  PASTEL_BLUE,
+  PASTEL_RED,
+  PASTEL_GREEN,
+  PASTEL_PURPLE,
+  PASTEL_YELLOW
+];
