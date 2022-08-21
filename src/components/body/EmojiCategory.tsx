6f48f0fa85408f17163b57b89f8543f92c35f45b
ealush <code@ealush.com>
2022-08-21 14:11:26 +0300

    Better Emoji Spacing

--- src/components/body/EmojiCategory.tsx
+++ src/components/body/EmojiCategory.tsx
@@ -1,21 +1,41 @@
 import * as React from 'react';
 import { DataGroups } from '../../dataUtils/DataTypes';
 import emojisByCategory from '../../dataUtils/emojisByCategory';
-import { Emoji } from './Emoji';
+import { useRef } from 'react';
 import './EmojiCategory.css';
+import { useIsEmojiFiltered } from '../../hooks/useFilter';
+import { emojiUnified } from '../../dataUtils/emojiSelectors';
+import { Emoji } from '../emoji/Emoji';
 
 type Props = Readonly<{
   category: DataGroups;
 }>;
 
 export function EmojiCategory({ category }: Props) {
+  const visibleEmojiCounter = useRef(0);
   const emojis = emojisByCategory(category);
+
+  visibleEmojiCounter.current = 0;
+
   return (
     <li className="epr-emoji-category" data-name={category}>
       <div className="epr-emoji-category-label">{category}</div>
-      {emojis.map(emoji => (
-        <Emoji key={emoji.u} emoji={emoji} />
-      ))}
+      {emojis.map(emoji => {
+        const hidden = useIsEmojiFiltered(emojiUnified(emoji));
+
+        if (!hidden) {
+          visibleEmojiCounter.current++;
+        }
+
+        return (
+          <Emoji
+            key={emoji.u}
+            emoji={emoji}
+            hidden={hidden}
+            index={visibleEmojiCounter.current}
+          />
+        );
+      })}
     </li>
   );
 }
