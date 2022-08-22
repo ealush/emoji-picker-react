b338dba7e34506c56e552716718c4ebca73bf396
ealush <code@ealush.com>
2022-08-23 01:51:43 +0300

    Add active category state to navigation component

--- src/components/header/CategoryNavigation.tsx
+++ src/components/header/CategoryNavigation.tsx
@@ -4,11 +4,14 @@ import clsx from 'clsx';
 import categories from '../../dataUtils/categories';
 
 export function CategoryNavigation() {
+  const [activeCategory] = React.useState(categories[0]);
   return (
     <div className="epr-category-nav">
       {categories.map(category => (
         <button
-          className={clsx('epr-cat-btn', `epr-icn-${category}`)}
+          className={clsx('epr-cat-btn', `epr-icn-${category}`, {
+            active: category === activeCategory
+          })}
           key={category}
         />
       ))}
