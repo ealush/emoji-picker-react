d829327b3ff1150939701842ce966ea6271fb6bd
ealush <code@ealush.com>
2022-08-26 01:08:37 +0300

    Add a generic scrollTo utility

--- src/DomUtils/scrollCategoryIntoView.ts
+++ src/DomUtils/scrollCategoryIntoView.ts
@@ -1,12 +1,9 @@
+import { scrollTo } from './scrollTo';
+
 export function scrollCategoryIntoView(
   root: HTMLElement | null,
   category: string
 ) {
-  const $eprBody = root ? root.querySelector('.epr-body') : null;
-
-  if (!root || !$eprBody) {
-    return;
-  }
   const $category = root?.querySelector(
     `[data-name="${category}"]`
   ) as HTMLElement;
@@ -17,7 +14,5 @@ export function scrollCategoryIntoView(
 
   const offsetTop = $category.offsetTop || 0;
 
-  requestAnimationFrame(() => {
-    $eprBody.scrollTop = offsetTop;
-  });
+  scrollTo(root, offsetTop);
 }
---
+++ src/DomUtils/scrollTo.ts
@@ -0,0 +1,11 @@
+export function scrollTo(root: HTMLElement | null, top: number = 0) {
+  const $eprBody = root ? root.querySelector('.epr-body') : null;
+
+  if (!root || !$eprBody) {
+    return;
+  }
+
+  requestAnimationFrame(() => {
+    $eprBody.scrollTop = top;
+  });
+}
