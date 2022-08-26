3de24c3e0d685d3ab29d14445a9652c12b7e8b92
ealush <code@ealush.com>
2022-08-26 22:01:57 +0300

    move category scroll detection to its own hook

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,51 +1,14 @@
 import * as React from 'react';
 import './Body.css';
 import { EmojiList } from './EmojiList';
-import { useEffect, useRef } from 'react';
-import { useActiveCategoryState } from '../context/PickerContext';
-import { categoryNameFromDom } from '../../DomUtils/categoryNameFromDom';
+import { useRef } from 'react';
 import { EmojiVariationPicker } from './EmojiVariationPicker';
+import { useActiveCategoryScrollDetection } from '../../hooks/useActiveCategoryScrollDetection';
 
 export function Body() {
   const bodyRef = useRef<null | HTMLDivElement>(null);
-  const [, setActiveCategory] = useActiveCategoryState();
 
-  useEffect(() => {
-    const visibleCategories = new Map();
-    const observer = new IntersectionObserver(
-      entries => {
-        const refCurrent = bodyRef.current;
-        if (!refCurrent) {
-          return;
-        }
-
-        for (const entry of entries) {
-          const id = categoryNameFromDom(entry.target);
-          visibleCategories.set(id, entry.intersectionRatio);
-        }
-
-        const ratios = Array.from(visibleCategories);
-        const lastCategory = ratios[ratios.length - 1];
-
-        if (lastCategory[1] == 1) {
-          return setActiveCategory(lastCategory[0]);
-        }
-
-        for (const [id, ratio] of ratios) {
-          if (ratio) {
-            setActiveCategory(id);
-            break;
-          }
-        }
-      },
-      {
-        threshold: [0, 1]
-      }
-    );
-    bodyRef.current?.querySelectorAll('.epr-emoji-category').forEach(el => {
-      observer.observe(el);
-    });
-  }, []);
+  useActiveCategoryScrollDetection(bodyRef);
 
   return (
     <div className="epr-body" ref={bodyRef}>
---
+++ src/hooks/useActiveCategoryScrollDetection.ts
@@ -0,0 +1,46 @@
+import { useActiveCategoryState } from '../components/context/PickerContext';
+import { useEffect } from 'react';
+import { categoryNameFromDom } from '../DomUtils/categoryNameFromDom';
+
+export function useActiveCategoryScrollDetection(
+  bodyRef: React.MutableRefObject<HTMLDivElement | null>
+) {
+  const [, setActiveCategory] = useActiveCategoryState();
+
+  useEffect(() => {
+    const visibleCategories = new Map();
+    const observer = new IntersectionObserver(
+      entries => {
+        const refCurrent = bodyRef.current;
+        if (!refCurrent) {
+          return;
+        }
+
+        for (const entry of entries) {
+          const id = categoryNameFromDom(entry.target);
+          visibleCategories.set(id, entry.intersectionRatio);
+        }
+
+        const ratios = Array.from(visibleCategories);
+        const lastCategory = ratios[ratios.length - 1];
+
+        if (lastCategory[1] == 1) {
+          return setActiveCategory(lastCategory[0]);
+        }
+
+        for (const [id, ratio] of ratios) {
+          if (ratio) {
+            setActiveCategory(id);
+            break;
+          }
+        }
+      },
+      {
+        threshold: [0, 1]
+      }
+    );
+    bodyRef.current?.querySelectorAll('.epr-emoji-category').forEach(el => {
+      observer.observe(el);
+    });
+  }, []);
+}
