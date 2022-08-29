5b086afa589f9d78aba16c73c498fb86f6b794e2
ealush <code@ealush.com>
2022-08-30 02:28:20 +0300

    Load svg with PostCss

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,12 +1,15 @@
 const postcss = require('rollup-plugin-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
 module.exports = {
   rollup(config, options) {
     config.plugins.push(
       postcss({
+        plugins: [
+          require('postcss-inline-svg'),
+          require('postcss-svgo'),
+          require('autoprefixer'),
+          require('cssnano')
+        ],
         inject: true,
-        // only write out CSS for the first bundle (avoids pointless extra files):
         extract: !!options.writeMeta
       })
     );
