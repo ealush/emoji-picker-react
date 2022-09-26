934fe8414a4e6aff8e33c6686eb153fdd44f626c
ealush <code@ealush.com>
2022-09-26 12:01:11 -0400

    Update npm keywords

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./emoji-picker-react.cjs.production.min.js')
-} else {
-  module.exports = require('./emoji-picker-react.cjs.development.js')
-}
