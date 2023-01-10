68f474f804234fb719db22b4f87dac64564a33b7
Nancy Eckenthal <nancy.eckenthal@gmail.com>
2023-01-10 11:37:39 -0500

    whoops - need to enable the new skin tone persistence via keyboard actions

--- src/components/header/SkinTonePicker.tsx
+++ src/components/header/SkinTonePicker.tsx
@@ -107,6 +107,7 @@ export function SkinTonePicker({
                 if (key === KeyboardEvents.Enter) {
                   if (isOpen) {
                     setActiveSkinTone(skinToneVariation);
+                    setSkinTone(skinToneVariation)
                     focusSearchInput();
                   } else {
                     setIsOpen(true);
