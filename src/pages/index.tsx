706ef8f398e67e84f848c0118b300af0e38a2037
Evyatar <code@ealush.com>
2023-10-24 01:09:32 +0100

    Components Folder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import PickerDemo from "./PickerDemo";
+import PickerDemo from "../components/PickerDemo";
 
 const inter = Inter({ subsets: ["latin"] });
 
