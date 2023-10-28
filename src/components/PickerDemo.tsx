3a032e1eb11c421185d619da0afb5025ed7a5a93
Evyatar <code@ealush.com>
2023-10-29 00:47:19 +0200

    No SSR for the Picker

--- src/components/PickerDemo.tsx
+++ src/components/PickerDemo.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Picker, {
   SkinTonePickerLocation,
   SuggestionMode,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,10 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import PickerDemo from "../components/PickerDemo";
+import dynamic from "next/dynamic";
+const PickerDemo = dynamic(() => import("../components/PickerDemo"), {
+  ssr: false,
+});
 
 const inter = Inter({ subsets: ["latin"] });
 
