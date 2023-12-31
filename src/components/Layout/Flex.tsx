8073d9d5493c2027865f6ee8ec0b680cb5eb6846
Evyatar <code@ealush.com>
2023-12-31 19:27:55 +0200

    flex

--- src/components/Layout/Flex.tsx
+++ src/components/Layout/Flex.tsx
@@ -1,12 +1,27 @@
+import { cx } from 'css-local';
 import * as React from 'react';
 
-import './Flex.css';
 import { clsx } from '../../DomUtils/classNames';
+import { sheet } from '../../DomUtils/stylesheet';
 
 export enum FlexDirection {
   ROW = 'FlexRow',
   COLUMN = 'FlexColumn'
 }
+
+const styles = sheet.create({
+  Flex: {
+    '.': 'Flex',
+    display: 'flex',
+    flexDirection: 'var(--epr-flex-direction, row)'
+  },
+  FlexRow: {
+    flexDirection: 'row'
+  },
+  FlexColumn: {
+    flexDirection: 'column'
+  }
+});
 
 type Props = Readonly<{
   children: React.ReactNode;
@@ -22,7 +37,16 @@ export default function Flex({
   direction = FlexDirection.ROW
 }: Props) {
   return (
-    <div style={{ ...style }} className={clsx('Flex', className, direction)}>
+    <div
+      style={{ ...style }}
+      className={clsx(
+        cx(
+          styles.Flex,
+          direction === FlexDirection.ROW ? styles.FlexRow : styles.FlexColumn
+        ),
+        className
+      )}
+    >
       {children}
     </div>
   );
