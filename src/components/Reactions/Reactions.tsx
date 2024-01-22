c6ae500909cb2512c043a03923db27262f0f410a
Evyatar <code@ealush.com>
2024-01-22 21:38:34 +0200

    Reactions: Use style from config

--- src/components/Reactions/Reactions.tsx
+++ src/components/Reactions/Reactions.tsx
@@ -2,11 +2,13 @@ import { cx } from 'flairup';
 import * as React from 'react';
 
 import { commonStyles, stylesheet } from '../../Stylesheet/stylesheet';
-import { useReactionsConfig } from '../../config/useConfig';
+import {
+  useEmojiStyleConfig,
+  useReactionsConfig
+} from '../../config/useConfig';
 import { DataEmoji } from '../../dataUtils/DataTypes';
 import { emojiByUnified } from '../../dataUtils/emojiSelectors';
 import { useMouseDownHandlers } from '../../hooks/useMouseDownHandlers';
-import { EmojiStyle } from '../../types/exposedTypes';
 import { useReactionsRef } from '../context/ElementRefContext';
 import { useReactionsModeState } from '../context/PickerContext';
 import { ClickableEmoji } from '../emoji/Emoji';
@@ -18,6 +20,7 @@ export function Reactions() {
   const ReactionsRef = useReactionsRef();
   const reactions = useReactionsConfig();
   useMouseDownHandlers(ReactionsRef);
+  const emojiStyle = useEmojiStyleConfig();
 
   if (!reactionsOpen) {
     return null;
@@ -32,10 +35,10 @@ export function Reactions() {
         <li key={reaction}>
           <ClickableEmoji
             emoji={emojiByUnified(reaction) as DataEmoji}
-            emojiStyle={EmojiStyle.NATIVE}
+            emojiStyle={emojiStyle}
             unified={reaction}
             showVariations={false}
-            round={true}
+            round
           />
         </li>
       ))}
