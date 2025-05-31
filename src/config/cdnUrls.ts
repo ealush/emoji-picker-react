0afc8504a7a4d1f0d47414bd4ddf3dcbb705856c
Halim <halimdev@gmail.com>
2025-05-31 00:39:00 -0400

    Use local emoji assets instead of CDN

--- src/config/cdnUrls.ts
+++ src/config/cdnUrls.ts
@@ -1,5 +1,34 @@
 import { EmojiStyle } from '../types/exposedTypes';
 
+// Local paths mirroring the CDN structure
+//const LOCAL_URL_APPLE = '/emoji-datasource-apple/img/apple/64/';
+//const LOCAL_URL_FACEBOOK = '/emoji-datasource-facebook/img/facebook/64/';
+//const LOCAL_URL_TWITTER = '/emoji-datasource-twitter/img/twitter/64/';
+//const LOCAL_URL_GOOGLE = '/emoji-datasource-google/img/google/64/';
+
+// Local paths mirroring the CDN structure
+const LOCAL_URL_APPLE = '/emoji/apple-emoji/64/';
+const LOCAL_URL_FACEBOOK = '/emoji/facebook-emoji/64/';
+const LOCAL_URL_TWITTER = '/emoji/twitter-emoji/64/';
+const LOCAL_URL_GOOGLE = '/emoji/google-emoji/64/';
+
+export function cdnUrl(emojiStyle: EmojiStyle): string {
+  switch (emojiStyle) {
+    case EmojiStyle.TWITTER:
+      return LOCAL_URL_TWITTER;
+    case EmojiStyle.GOOGLE:
+      return LOCAL_URL_GOOGLE;
+    case EmojiStyle.FACEBOOK:
+      return LOCAL_URL_FACEBOOK;
+    case EmojiStyle.APPLE:
+    default:
+      return LOCAL_URL_APPLE;
+  }
+}
+
+//import { EmojiStyle } from '../types/exposedTypes';
+
+/*
 const CDN_URL_APPLE =
   'https://cdn.jsdelivr.net/npm/emoji-datasource-apple/img/apple/64/';
 const CDN_URL_FACEBOOK =
@@ -22,3 +51,14 @@ export function cdnUrl(emojiStyle: EmojiStyle): string {
       return CDN_URL_APPLE;
   }
 }
+*/
+
+//const LOCAL_URL = '/twitter-emoji/64/';
+
+//export function cdnUrl(emojiStyle: EmojiStyle): string {
+  //return LOCAL_URL; // Always use local path
+//}
+
+//export function cdnUrl(): string {
+  //return LOCAL_URL;
+//}
