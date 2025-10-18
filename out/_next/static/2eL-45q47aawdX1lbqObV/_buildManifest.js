fec85b071cb977cd595e933641e3813a9505c4f4
ealush <ealush@users.noreply.github.com>
2025-10-18 17:24:32 +0000

    updating package

---
+++ out/_next/static/2eL-45q47aawdX1lbqObV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/48d448c8-ba8657f1193a5898.js","static/css/daedbc49d8744239.css","static/chunks/pages/index-540fc4e319f4abc1.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
