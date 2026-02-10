e8bc00c56b34028c8d84647cb51d7e4d83b37bb9
Evyatar <code@ealush.com>
2026-02-10 14:30:28 +0200

    Add Google Analytics gtag to Next app (#501)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-import { GTAG } from "@/components/GTag";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -8,7 +7,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <GTAG />
       </body>
     </Html>
   );
